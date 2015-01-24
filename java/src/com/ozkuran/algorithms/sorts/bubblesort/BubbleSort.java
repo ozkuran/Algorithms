9e005dac40e078922dfd545aa19c86d3dac472bf
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-01-24 11:48:47 +0200

    Implemented Bubble Sort

---
+++ java/src/com/ozkuran/algorithms/sorts/bubblesort/BubbleSort.java
@@ -0,0 +1,32 @@
+package com.ozkuran.algorithms.sorts.bubblesort;
+
+/**
+ * @version 0.0.6.23
+ * Created by MahmutAli on 11/9/2009.
+ * Last Update by MahmutAli on 1/24/2015.
+ * Returns if given number is a Prime Number
+ */
+public class BubbleSort<T extends Comparable<T>> {
+
+    public static <T extends Comparable<? super T>> T[] sort(T[] input) {
+        boolean swapped = true;
+        int length = input.length;
+        while (swapped) {
+            swapped = false;
+            for (int i = 1; i < length; i++) {
+                if (input[i].compareTo(input[i - 1]) < 0) {
+                    swap(i, i - 1, input);
+                    swapped = true;
+                }
+            }
+            length--;
+        }
+        return input;
+    }
+
+    private static <T extends Comparable<? super T>> void swap(int index1, int index2, T[] input) {
+        T value = input[index1];
+        input[index1] = input[index2];
+        input[index2] = value;
+    }
+}
---
+++ java/src/com/ozkuran/algorithms/sorts/bubblesort/BubbleSortTest.java
@@ -0,0 +1,29 @@
+package com.ozkuran.algorithms.sorts.bubblesort;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class BubbleSortTest extends TestCase {
+
+    public void testSortsSmallIntegerArray() throws Exception {
+        Integer[] input = {4,2,5,1};
+        Integer[] sorted = {1,2,4,5};
+        Integer[] output = null;
+
+        output = BubbleSort.sort(input);
+        //assertSame(sorted,output);
+        assertArrayEquals(sorted, output);
+    }
+
+
+    public void testSortsSmallDoubleArray() throws Exception {
+        Double[] input = {4.0,2.0,5.5,1.0};
+        Double[] sorted = {1.0,2.0,4.0,5.5};
+        Double[] output = null;
+
+        output = BubbleSort.sort(input);
+        //assertSame(sorted,output);
+        assertArrayEquals(sorted, output);
+    }
+}
