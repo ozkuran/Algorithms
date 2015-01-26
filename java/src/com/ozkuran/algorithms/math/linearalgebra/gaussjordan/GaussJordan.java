15bfd0db1cf5991e6a60e9f6fac0eb2da6358ad9
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-01-26 07:20:20 +0200

    Initialization of Gauss Jordan

---
+++ java/src/com/ozkuran/algorithms/math/linearalgebra/gaussjordan/GaussJordan.java
@@ -0,0 +1,13 @@
+package com.ozkuran.algorithms.math.linearalgebra.gaussjordan;
+
+import static java.lang.Math.abs;
+
+/**
+ * @version 0.0.3.34
+ * Created by MahmutAli on 11/9/2010.
+ * Last Update by MahmutAli on 1/24/2015.
+ */
+
+public class GaussJordan {
+
+}
---
+++ java/src/com/ozkuran/algorithms/math/linearalgebra/gaussjordan/GaussJordanTest.java
@@ -0,0 +1,7 @@
+package com.ozkuran.algorithms.math.linearalgebra.gaussjordan;
+
+import junit.framework.TestCase;
+
+public class GaussJordanTest extends TestCase {
+
+}
