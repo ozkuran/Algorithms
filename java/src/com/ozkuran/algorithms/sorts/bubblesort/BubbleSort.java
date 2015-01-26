c6a5c4b724f96d6d9de230ffa9440efd344d29eb
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-01-26 11:47:49 +0200

    Fixed description of Bubble Sort

--- java/src/com/ozkuran/algorithms/sorts/bubblesort/BubbleSort.java
+++ java/src/com/ozkuran/algorithms/sorts/bubblesort/BubbleSort.java
@@ -4,9 +4,12 @@ package com.ozkuran.algorithms.sorts.bubblesort;
  * @version 0.0.6.23
  * Created by MahmutAli on 11/9/2009.
  * Last Update by MahmutAli on 1/24/2015.
- * Returns if given number is a Prime Number
+ * Returns if given Array in sorted order with Bubble Sort
+ * Worst Case Performance O(n.n)
+ * Best Case Performance O(n)
+ * Average Case Performance O(n.n)
  */
-public class BubbleSort<T extends Comparable<T>> {
+public abstract class BubbleSort<T extends Comparable<T>> {
 
     public static <T extends Comparable<? super T>> T[] sort(T[] input) {
         boolean swapped = true;
