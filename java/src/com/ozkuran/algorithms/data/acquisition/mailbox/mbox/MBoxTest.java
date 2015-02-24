131469951272776c7ed9639e9100f8ac8bc2dc2a
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-02-24 13:52:41 +0200

    Added initial MBoxTest class

---
+++ java/src/com/ozkuran/algorithms/data/acquisition/mailbox/mbox/MBoxTest.java
@@ -0,0 +1,10 @@
+package com.ozkuran.algorithms.data.acquisition.mailbox.mbox;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by MahmutAli on 2/24/2015.
+ */
+public class MBoxTest  extends TestCase {
+
+}
