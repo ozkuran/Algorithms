027d03085d9e8220ae0f27044af2a91a92657ac5
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-02-24 13:55:07 +0200

    Added initial Bİsection class

---
+++ java/src/com/ozkuran/algorithms/math/approximation/bisection/Bisection.java
@@ -0,0 +1,10 @@
+package com.ozkuran.algorithms.math.approximation.bisection;
+
+/**
+ * @version 0.0.1
+ * Created by MahmutAli on 2/24/2015.
+ * Last Update by MahmutAli on 2/24/2015.
+ * Calculates Approximation of a Polynomial using Bisection Algorithm
+ */
+public class Bisection {
+}
