8f0aa404674866d408c6fb896a83c8da01e6af67
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-02-26 02:01:05 +0200

    Initial

--- java/src/com/ozkuran/algorithms/security/checksum/tendigitisbncheck/TenDigitISBNCheck.java
+++ java/src/com/ozkuran/algorithms/security/checksum/tendigitisbncheck/TenDigitISBNCheck.java
@@ -24,7 +24,7 @@ public class TenDigitISBNCheck {
         }
         int modSum = 0;
         for(int i = 0; i < size; i++){
-            modSum += _isbnNumber.get(i) * (size - i);
+                modSum += _isbnNumber.get(i) * (size - i);
         }
         if ((modSum % 10) == 0){
             result = true;
